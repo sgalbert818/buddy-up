eaf01d20404f81530dfd19488ce13e2eb227cdf1
Sarah Albert <sgalbert818@yahoo.com>
2025-07-14 12:26:55 -0600

    updating user home page

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+export default function Main() {
+
+    return (
+        <div className="main">
+            <h3>No users to show. Try again later.</h3>
+            <div className="main-btns">
+                <button>X</button>
+                <button>✓</button>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,15 +2,7 @@ import { useUser } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useDelete } from '../hooks/useDelete';
-
-/* interface Error {
-    response: {
-        data: {
-            message: string
-        }
-    }
-    status: number
-} */
+import Main from '../components/Main'
 
 export default function Home() {
 
@@ -24,28 +16,15 @@ export default function Home() {
     }
 
     const test = async () => {
-        /* if (token) {
-            try {
-                const config = {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        'Content-Type': 'application/json', // Ensure it's treated as JSON
-                    }
-                };
-                const response = await axios.post('http://localhost:5000/test', 'placeholder', config);
-                const userProfile = response.data
-                console.log('Response from backend:', userProfile);
-            } catch (err) {
-                const error = err as Error;
-                alert(error.response.data.message)
-            }
-        }*/
         console.log(userProfile)
     };
 
     return (
         <div>
             <h3>Welcome {userProfile?.name || ''}</h3>
+            <div>
+                <Main />
+            </div>
             <button onClick={test}>Test</button>
             <button onClick={editProfile}>Edit Profile</button>
             <button onClick={logout}>Sign Out</button>
